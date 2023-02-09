f0e42373da09c330719f78090e9a7bba564a7c91
Matthew Young <myoung.space.science@gmail.com>
2023-02-09 12:34:26 -0500

    Initial commit of existing files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+Create and modify groups of EPREM runs via the Python interpreter.
+"""
+
+from ._interface import Interface as Interface
+
---
+++ __main__.py
@@ -0,0 +1,8 @@
+"""
+Create and modify groups of EPREM runs via the command line.
+"""
+
+import _interface
+
+_interface.cli.run()
+
